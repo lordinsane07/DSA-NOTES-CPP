8a4d7501e27061b6dd5f2e0fffa7fed7e50dae28
Mrittick Deb <147146704+lordinsane07@users.noreply.github.com>
2026-02-21 14:54:58 +0530

    Add solution for H-Index

---
+++ LeetCode/H-Index/Solution.cpp
@@ -0,0 +1,10 @@
+Definition says "been cited at least h times " 
+ so, 4 papers, at index 3, has been cited at least 1 times
+4 papers can't be cited  at least 4 times or at least 3 times or any number > the minimum citation we see at this index, viz  1
+
+_Hence, we have to take min(citation[i], index+1) for h-idx_
+(0-index) h-idx = min(4, 1)
+
+We don;t stop here, idea is to MAXIMIZE  h-Index and we have to Try every Index in this fashion Uptil Maximum Allowed index
+
+Basically we have the maximize the last column in table above^^
