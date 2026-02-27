def1f2aa1af71f78a22d6678d140b6baec79f034
Mrittick Deb <147146704+lordinsane07@users.noreply.github.com>
2026-02-28 00:17:51 +0530

    Add solution for Number of Steps to Reduce a Number in Binary Representation to One

--- LeetCode/Number-of-Steps-to-Reduce-a-Number-in-Binary-Representation-to-One/Solution.cpp
+++ LeetCode/Number-of-Steps-to-Reduce-a-Number-in-Binary-Representation-to-One/Solution.cpp
@@ -1,8 +1,29 @@
-func numSteps(s string) int {
-	steps, carry := 0, byte(0)
-	for i := len(s) - 1; i > 0; i-- {
-		steps += int(s[i]+carry)&1 + 1
-		carry |= s[i] & 1
-	}
-	return steps + int(carry)
-}
+1class Solution {
+2public:
+3    int numSteps(string s) {
+4   int steps=0,carry=0;
+5   for(int i=s.length()-1;i>=1;i--){
+6    int bit=s[i] & 1;
+7    steps+=1+(bit ^ carry);
+8    carry |=bit;
+9   }
+10   return steps+carry;
+11
+12    //     int n=s.length();
+13    //    __int128 sum=0;
+14    //     int k=0;
+15    //     for(int i=0;i<n;i++){
+16    //             if(s[i]=='1'){
+17    //                 sum+=((__int128)1 <<k);
+18    //             }
+19    //             k++;
+20    //     }
+21    //      long long count=0;
+22    //     while(sum!=0){
+23    //         if(sum%2==1) {sum+=1;}
+24    //         else {sum/=2;}
+25    //       count++;
+26    //     }
+27    //     return count;
+28    }
+29};
