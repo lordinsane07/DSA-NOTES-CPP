0a1169c8bf0aeec011a0236f517582df85daea81
Mrittick Deb <147146704+lordinsane07@users.noreply.github.com>
2026-03-18 21:50:01 +0530

    Time: 62 ms (82.95%) | Memory: 172.9 MB (86.98%) - LeetSync

---
+++ 146-lru-cache/lru-cache.cpp
@@ -0,0 +1,73 @@
+class Node{
+    public:
+    int key,val;
+    Node* next;
+    Node* prev;
+    Node(){
+        key=val=-1;
+    next=prev=nullptr;
+    }
+    Node(int k,int value){
+            key=k;
+            val=value;
+             next=prev=nullptr;
+    }
+};
+class LRUCache {
+    unordered_map<int,Node*> mpp;
+    int cap;
+    Node* head;
+    Node* tail;
+    void deleteNode(Node* node){
+        Node* prevNode=node->prev;
+        Node* nextNode=node->next;
+        prevNode->next=nextNode;
+        nextNode->prev=prevNode;
+
+    }
+    void insertNode(Node* node){
+      Node* nextNode=head->next;
+      head->next=node;
+      nextNode->prev=node;
+      node->prev=head;
+      node->next=nextNode;
+
+    }
+  public:
+
+  LRUCache(int capacity) {
+    cap=capacity;
+    mpp.clear();
+    head=new Node();
+    tail=new Node();
+    head->next=tail;
+    tail->prev=head;
+  }
+
+  int get(int key_) {
+        if(mpp.find(key_)==mpp.end()) return -1;
+        Node* node=mpp[key_];
+        int val=node->val;
+        deleteNode(node);
+        insertNode(node);
+        return val;
+  }
+
+  void put(int key_, int value) {
+    if(mpp.find(key_)!=mpp.end()){
+        Node* node=mpp[key_];
+        node->val=value;
+        deleteNode(node);
+        insertNode(node);
+        return;
+    }
+    if(mpp.size()==cap){
+        Node* node=tail->prev;
+        mpp.erase(node->key);
+        deleteNode(node);
+    }
+    Node* newnode=new Node(key_,value);
+    mpp[key_]=newnode;
+    insertNode(newnode);
+  }
+};
