aa09326cb252066733831429a9e64ca40253d671
Mrittick Deb <147146704+lordinsane07@users.noreply.github.com>
2026-03-24 14:34:24 +0530

    Add solution for Product of Array Except Self

--- LeetCode/Product-of-Array-Except-Self/Solution.cpp
+++ LeetCode/Product-of-Array-Except-Self/Solution.cpp
@@ -1,21 +1,19 @@
 1class Solution {
 2public:
-3    vector<int> productExceptSelf(vector<int>& nums) {//FOR EVERY ELEMENT=>(PRODUCT OF ALL OTHER ELEMENT EXCEPT ITSELF)=>product of all other in left part * product of all other in right part 
+3    vector<int> productExceptSelf(vector<int>& nums) {
 4        int n=nums.size();
 5        vector<int> pre(n);
-6        //prefix product array
-7        int p=nums[0];
-8        pre[0]=1;
-9        for(int i=1;i<n;i++){
-10            pre[i]=p;
-11            p *=nums[i];
-12        }
-13        //suffix product array(in prefix only)
-14        p=nums[n-1];
-15        for(int i=n-2;i>=0;i--){
-16            pre[i] *=p;
-17            p  *=nums[i];
-18        }
-19        return pre;
-20    }
-21};
+6        int p=nums[0];
+7        pre[0]=1;
+8        for(int i=1;i<n;i++){
+9            pre[i]=p;
+10            p*=nums[i];
+11        }
+12        p=nums[n-1];
+13        for(int i=n-2;i>=0;i--){
+14            pre[i]*=p;
+15            p=p*nums[i];
+16        }
+17        return pre;
+18    }
+19};
