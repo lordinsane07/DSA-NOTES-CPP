ae4031944c38f2dd9465f935a01af1e2969b0b0b
Mrittick Deb <147146704+lordinsane07@users.noreply.github.com>
2026-04-01 17:17:24 +0530

    Time: 2 ms (15.88%) | Memory: 13.2 MB (54.38%) - LeetSync

---
+++ 120-triangle/triangle.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+int func(vector<vector<int>>& dp,int n,vector<vector<int>>& triangle){
+       for(int i=0;i<n;i++) dp[n-1][i]=triangle[n-1][i];
+       for(int i=n-2;i>=0;i--){
+        for(int j=i;j>=0;j--){
+                  int down=triangle[i][j]+dp[i+1][j];
+        int diagonal=triangle[i][j]+dp[i+1][j+1];
+        dp[i][j]=min(down,diagonal);
+        }
+       }
+      
+        return dp[0][0];
+    }
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n=triangle.size();
+        vector<vector<int>> dp(n,vector<int> (n,-1));
+        return func(dp,n,triangle);
+    }
+   
+};
