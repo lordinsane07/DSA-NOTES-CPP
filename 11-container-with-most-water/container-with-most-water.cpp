4b2342dfdba8d59a74d66e4272e3be3d2503e2f0
Mrittick Deb <147146704+lordinsane07@users.noreply.github.com>
2026-04-21 22:23:36 +0530

    Time: 0 ms (100.00%) | Memory: 63 MB (47.58%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {
+        int n=height.size();
+        int i=0,j=n-1;
+        int maxlen=INT_MIN;
+        while(i<j){
+            int width=j-i;
+            int heights=min(height[i],height[j]);
+            maxlen=max(maxlen,width*heights);
+            if(height[i]<height[j]) i++;
+            else j--;
+        }
+        return maxlen;
+    }
+};
